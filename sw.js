d229726ca4c1d4e8ae2f7854f0a526f2dbf53f03
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2025-08-10 22:25:14 +0800

    a

---
+++ sw.js
@@ -0,0 +1,10 @@
+// No-cache service worker
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
