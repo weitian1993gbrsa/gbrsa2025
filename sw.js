645cdd8aaf5e4911fdb888bc6d197f4ec0379523
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2025-08-10 22:19:33 +0800

    a

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gbrsa-pwa-v9";
+const CACHE = "gbrsa-pwa-v11";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,8 +7,13 @@ const ASSETS = [
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
+
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(clients.claim());
 });
 self.addEventListener("fetch", e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
