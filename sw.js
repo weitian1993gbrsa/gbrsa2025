bb240eb12213c416c5ee31ab51cb2ecdc6d3a94b
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2025-08-10 22:00:01 +0800

    A

--- sw.js
+++ sw.js
@@ -1,35 +1,15 @@
-
-const CACHE = "gbrsa-pwa-v4.2";
+const CACHE = "gbrsa-pwa-v5";
 const ASSETS = [
   "./",
   "./index.html",
+  "./speed/index.html",
   "./manifest.webmanifest",
-  "./logo.png",
-  "./icons/icon-192-v4.png",
-  "./icons/icon-512-v4.png",
-  "./icons/icon-192-maskable-v4.png",
-  "./icons/icon-512-maskable-v4.png",
-  "./icons/apple-touch-icon-v4.png",
-  "./icons/favicon-32-v4.png"
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))),
-  );
-  self.clients.claim();
-});
-self.addEventListener("fetch", (e) => {
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return resp;
-      }))
-    );
-  }
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
