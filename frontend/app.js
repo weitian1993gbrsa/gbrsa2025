2c20101a3be3c2e8edec1c2b3638eff4034920f7
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2025-08-27 11:32:32 +0800

    a

--- frontend/app.js
+++ frontend/app.js
@@ -81,7 +81,7 @@ setInterval(checkForDataUpdates, 30000);
 checkForDataUpdates();
 
 // === LOGIN SYSTEM ===
-const scriptURL = "https://SCRIPT_URL_HERE/exec";  // REPLACE with your real Web App URL
+const scriptURL = "https://script.google.com/macros/s/AKfycbyPFNdVkdZO5_Gu4TaeFTc1fbH1541HTXtOxyzQrYY90dcPKJ6KlswHB9ru_xZfE26EGg/exec";  // REPLACE with your real Web App URL
 
 function showLogin() {
   const loginHTML = \`
