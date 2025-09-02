ac777788b65076485036a0d3fad8007b543b0469
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2025-09-02 12:45:08 +0800

    a

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // Backend URL for GBRSA Judge App
-const BACKEND_URL = "https://script.google.com/macros/s/AKfycbyPFNdVkdZO5_Gu4TaeFTc1fbH1541HTXtOxyzQrYY90dcPKJ6KlswHB9ru_xZfE26EGg/exec";
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbxEOADfwC0ipkslwJTWNcbtMpXkaxZkCTtyvXj4jZbhZVGCF02bOpQk1SlHpRciYoyLEQ/exec";
