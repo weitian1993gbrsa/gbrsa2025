79fff28250a9c279b585ef02d9628e2d8d522ba5
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2025-09-04 11:27:06 +0800

    a

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyPFNdVkdZO5_Gu4TaeFTc1fbH1541HTXtOxyzQrYY90dcPKJ6KlswHB9ru_xZfE26EGg/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxEOADfwC0ipkslwJTWNcbtMpXkaxZkCTtyvXj4jZbhZVGCF02bOpQk1SlHpRciYoyLEQ/exec",
   SHEET_ID: "1jJzY7YPWp2z--NoA9zjegzss4ZJXH4_eTuaePmHe0dg",
   DATA_SHEET_NAME: "Data",
   RESULT_SHEET_NAME: "Result"
