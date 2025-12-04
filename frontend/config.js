5e4e88de79cb1adbe357f5f045a87f5a4f92c358
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2025-12-04 19:27:36 +0800

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,19 +1,14 @@
 /* ============================================================
-   GLOBAL CONFIG  (Speed + Freestyle Support)
+   GLOBAL CONFIG
 ============================================================ */
 
 window.CONFIG = {
-  APPS_SCRIPT_URL:
-    "https://script.google.com/macros/s/AKfycbw1UORMl0LqZPdxelb8DdWSKZUUcX3WL03GDUSM-GeLX9v-KYHeeeFwQ8epyS3va3RQTA/exec",
-
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw1UORMl0LqZPdxelb8DdWSKZUUcX3WL03GDUSM-GeLX9v-KYHeeeFwQ8epyS3va3RQTA/exec",
   SHEET_ID: "1jJzY7YPWp2z--NoA9zjegzss4ZJXH4_eTuaePmHe0dg",
-
   DATA_SHEET_NAME: "DATA",
-
   RESULT_SHEET_NAME: "RESULT",
   RESULT_F_SHEET_NAME: "RESULT_F"
 };
-
 
 /* ============================================================
    LOGIN BACKEND URL
@@ -22,9 +17,8 @@ window.CONFIG = {
 const LOGIN_API_URL =
   "https://script.google.com/macros/s/AKfycbzq3LYbSAoFRjt8wIbomxwehCOdAdGo4eCkpHlhj_ncTm27tpQJyMhzQHRQTfhKQ6WJ/exec";
 
-
 /* ============================================================
-   SPEED EVENT GROUPS
+   EVENT GROUPS
 ============================================================ */
 
 window.SPEED_EVENTS = [
@@ -36,26 +30,17 @@ window.SPEED_EVENTS = [
   "SRSR"
 ];
 
-
-/* ============================================================
-   FREESTYLE EVENT GROUPS
-============================================================ */
-
 window.FREESTYLE_EVENTS = [
   "SRIF",
   "SRIF_LEVEL 1",
   "SRIF_LEVEL 2"
 ];
 
-
 /* ============================================================
    JUDGE ACCESS KEYS
-   (Speed + Freestyle, all stations)
 ============================================================ */
 
 window.JUDGE_KEYS = {
-
-  // SPEED
   "abc123": { event: "speed", station: 1 },
   "def456": { event: "speed", station: 2 },
   "ghi789": { event: "speed", station: 3 },
@@ -69,22 +54,18 @@ window.JUDGE_KEYS = {
   "qqq101": { event: "speed", station: 11 },
   "key999": { event: "speed", station: 12 },
 
-  // FREESTYLE station 1
   "fd1": { event: "freestyle", station: 1, judgeType: "difficulty" },
   "ft1": { event: "freestyle", station: 1, judgeType: "technical" },
   "fp1": { event: "freestyle", station: 1, judgeType: "presentation" },
 
-  // FREESTYLE station 2
   "fd2": { event: "freestyle", station: 2, judgeType: "difficulty" },
   "ft2": { event: "freestyle", station: 2, judgeType: "technical" },
   "fp2": { event: "freestyle", station: 2, judgeType: "presentation" },
 
-  // FREESTYLE station 3
   "fd3": { event: "freestyle", station: 3, judgeType: "difficulty" },
   "ft3": { event: "freestyle", station: 3, judgeType: "technical" },
   "fp3": { event: "freestyle", station: 3, judgeType: "presentation" }
 };
-
 
 /* ============================================================
    ADMIN LOGIN KEY
